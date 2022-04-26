af589b2c353547b918b9431ac187db31aa7b19ae
Travis Ramcharran <travisramcharran@Traviss-MacBook-Air.local>
2022-04-26 01:57:44 -0500

    fxns, variables, console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName =  window.prompt("What is your robot's name?");
+// note the lack of quotation marks around playerName
+// what is this?
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+
+console.log(10 + 10);
+
+//what is this?
+
+console.log("Our robot's name is " + playerName);
+
+
+
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The FIGHT has BEGUN!")
+}
+//fight();
+
+
+
