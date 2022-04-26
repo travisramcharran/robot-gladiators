16d10c20784ec876b3c3c9c5756d3cfbefe695ed
Travis Ramcharran <travisramcharran@Traviss-MacBook-Air.local>
2022-04-26 18:17:52 -0500

    module 3 lesson 1

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,10 @@ var playerHealth = 100;
 
 var playerAttack = 10;
 
+var playerMoney = 10;
+
 // I can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
+console.log(playerName, playerAttack, playerHealth, playerMoney);
 
 var enemyName = "Roborto";
 //the enemyName variable is a String data type
@@ -24,38 +26,75 @@ var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
-    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in 'enemyHealth' variable.
-    enemyHealth = enemyHealth - playerAttack;
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or SKIP to choose.");
+    // to read back what the user input, use console.log, as shown below
+    console.log(promptFight);
 
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    //if player choses to fight, then fight
+ if (promptFight === "fight" || promptFight == "FIGHT") 
+    {
+
+        //remove enemys health by subtracting the amount set in the playerAttack variable
+     enemyHealth = enemyHealth - playerAttack;
+
+     // Log a resulting message to the console so we know that it worked.
+     console.log(
+         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+     );
     
-    );
-
-    //check enemy's health
-    if (enemyHealth <= 0) {
+     //check enemy's health
+        if (enemyHealth <= 0) {
         window.alert(enemyName + " had died!");
-    }
-
-    else {
+        } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left. ");
-    }
+        }
+     // Subtract the value of 'enemyAttack from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+     playerHealth = playerHealth - enemyAttack
 
-    // Subtract the value of 'enemyAttack from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-        playerHealth = playerHealth - enemyAttack
-
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
+     // Log a resulting message to the console so we know that it worked.
+     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
-    // check players health
-    if (playerHealth <= 0) {
+     );
+    
+     // check players health
+     if (playerHealth <= 0) {
         window.alert(playerName + " has died! ");
-    }
-    else {
+     }
+     else {
         window.alert(playerName + " still has " + playerHealth + " health left. ");
-    }
+     }
+    
+     //if player choses to skip
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        //confirm player wants to skip
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        //if yes (true), leave fight
+        if (confirmSkip) {
+        window.alert(playerName + " has decided to skip the fight. Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
+        console.log(playerMoney);
+        }
+        //if no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
+    } 
+     
+     else {
+        window.alert("You need to choose a valid option. Try Again!");
+     }
+    
+    
+    
+
+     
+
+    
+
+    
+    
 };
 // this executes the function
 fight();
@@ -63,15 +102,11 @@ fight();
 // this will show the players name in the console tab
 console.log(playerName);
 
-console.log("this logs a string, good for leaving yourself a message");
-
-//this will do math and log 20
-
-console.log(10 + 10);
 
 //this is a string concatenation and will show the players name as 'Our robots name is 'player name', which in this case is Tony.
-
+console.log("this logs a string, good for leaving yourself a message");
 console.log("Our robot's name is " + playerName);
+
 
 
 
@@ -80,13 +115,6 @@ console.log("Our robot's name is " + playerName);
 
 function fight() {
     window.alert("The FIGHT has BEGUN!")
+
 }
 //fight();
-
-
-
-// this will be 10, a number data type
-console.log(4 + 6);
-
-// this will be 46, a string data type
-console.log("4" + 6);
